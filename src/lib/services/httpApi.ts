93aa2b2cdf28ab66460c48066f8a438c2695e355
Alberto R. Noguez <betodevq@gmail.com>
2024-01-23 12:32:10 -0300

    Update fetch URL in httpApi.ts

--- src/lib/services/httpApi.ts
+++ src/lib/services/httpApi.ts
@@ -3,7 +3,7 @@ const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 const baseFetch = {
   get: async <T>(endpoint: string): Promise<T> => {
     try {
-      const response = await fetch(`${BASE_URL}${endpoint}`);
+      const response = await fetch(endpoint);
 
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
