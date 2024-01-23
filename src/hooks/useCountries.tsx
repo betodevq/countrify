d780283c74b6ab70a0a936747db82f0507ba359f
Alberto R. Noguez <betodevq@gmail.com>
2024-01-23 13:11:39 -0300

    Refactor handleCountryClick function in useCountries hook

--- src/hooks/useCountries.tsx
+++ src/hooks/useCountries.tsx
@@ -64,9 +64,9 @@ export const useCountries = () => {
   const handleCountryClick = async (newCode: string) => {
     if (newCode === country?.code) return;
     try {
-      const countryQuery = await getCountry({ variables: { code: newCode } });
-      if (countryQuery?.data?.country) {
-        setCountry(countryQuery.data.country);
+      const { data } = await getCountry({ variables: { code: newCode } });
+      if (data) {
+        setCountry(data.country);
       }
     } catch (error) {
       console.error("Error refetching country:", error);
